ab686a692ee6c5ba843720b0003d966083aa834c
house-of-coral- <houseofcoral258@gmail.com>
2026-07-03 20:06:07 +0100

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: String,
+    price: { type: Number, required: true },
+    sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Product', productSchema);
