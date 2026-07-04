cb85d4c77baf6949da6fba90eca97833a2c9a790
house-of-coral- <houseofcoral258@gmail.com>
2026-07-04 16:51:48 +0100

    Delete Bazaar.js

--- models/Bazaar.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const BazaarSchema = new mongoose.Schema({
-    itemName: String,
-    price: Number,
-    sellerId: String
-});
-module.exports = mongoose.model('Bazaar', BazaarSchema);
