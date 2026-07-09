d347fd459d087502ef9ee681322dca5fb8050f9b
house-of-coral <houseofcoral258@gmail.com>
2026-07-09 10:29:36 +0100

    Delete routes/bazaar.js
    Signed-off-by: house-of-coral <houseofcoral258@gmail.com>

--- routes/bazaar.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bazaarController = require('../controllers/bazaarController');
-
-router.post('/add', bazaarController.createItem);
-module.exports = router;
