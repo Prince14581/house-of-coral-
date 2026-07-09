f2e95ee9c3f013e216c34acbd179b54ccf3b2a5d
house-of-coral <houseofcoral258@gmail.com>
2026-07-09 10:57:00 +0100

    Delete routes/stage.js
    Signed-off-by: house-of-coral <houseofcoral258@gmail.com>

--- routes/stage.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const stageController = require('../controllers/stageController');
-
-// Route to check stream status
-router.get('/status', stageController.getStreamStatus);
-
-module.exports = router;
